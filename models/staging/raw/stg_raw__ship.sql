913a370ff47d94cb2baaa2c4e1d50efba090a7fd
evacharbit44@gmail.com <evacharbit44@gmail.com>
2025-11-05 10:14:00 +0000

    commit

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,12 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        shipping_fee_1,
         logcost,
-        CAST(ship_cost as float64) as ship_cost
+        ship_cost
 
     from source
 
 )
 
 select * from renamed
-
